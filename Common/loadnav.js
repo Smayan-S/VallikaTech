043a687b285adb4684d85f3aa6cf3a18a2a32b83
Smayan-S <147726764+Smayan-S@users.noreply.github.com>
2025-07-26 12:25:41 +0530

    Update loadnav.js

--- Common/loadnav.js
+++ Common/loadnav.js
@@ -1,4 +1,4 @@
-fetch('/VallikaTech/Common/navbar.html')
+fetch('VallikaTech/Common/navbar.html')
   .then(res => res.text())
   .then(data => {
     document.getElementById('navbar').innerHTML = data;
